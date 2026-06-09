aaccdfd75936e0209d260c86a3a876f30fd77663
gio <gio@GIOPC>
2026-06-09 12:07:57 +0400

    feat: add dockerfile

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/lcm", (string x, string y) =>
+app.MapGet("abesdze_giorgi_gmail_com", (string x, string y) =>
 {
-    if (!long.TryParse(x, out long a) || !long.TryParse(y, out long b) || a <= 0 || b <= 0)
+    if (!long.TryParse(x, out long a) ||
+        !long.TryParse(y, out long b) ||
+        a <= 0 ||
+        b <= 0)
+    {
         return "NaN";
+    }
 
-    return LCM(a, b).ToString();
+    long Gcd(long m, long n)
+    {
+        while (n != 0)
+        {
+            (m, n) = (n, m % n);
+        }
+
+        return m;
+    }
+
+    return ((a / Gcd(a, b)) * b).ToString();
 });
 
 app.Run();
-
-long GCD(long a, long b)
-{
-    while (b != 0)
-    {
-        long t = b;
-        b = a % b;
-        a = t;
-    }
-    return a;
-}
-
-long LCM(long a, long b)
-{
-    return (a / GCD(a, b)) * b;
-}
